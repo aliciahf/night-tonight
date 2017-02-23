f9e311c402ccff11550332fb4ea0aed541c8fdea
sandra honigman <sphonigman@gmail.com>
2017-02-22 23:05:05 -0500

    View change log 1.2.1 to see all changes

--- SexxxySiren1/SexxxySiren1/Assets/Scripts/playerMovement2.cs
+++ SexxxySiren1/SexxxySiren1/Assets/Scripts/playerMovement2.cs
@@ -4,11 +4,17 @@ using System.Collections;
 public class playerMovement2 : MonoBehaviour {
 
 	public float playerSpeed = 1f;
-	public KeyCode up = KeyCode.UpArrow;
-	public KeyCode down = KeyCode.DownArrow;
-	public KeyCode right = KeyCode.RightArrow; 
-	public KeyCode left = KeyCode.LeftArrow; 
-	Animator animator;
+    public KeyCode up;
+    public KeyCode down;
+	public KeyCode right; 
+	public KeyCode left;
+
+    public KeyCode up2;
+    public KeyCode down2;
+    public KeyCode right2;
+    public KeyCode left2;
+
+    Animator animator;
 
 	void Start (){
 		animator = GetComponent<Animator>();
@@ -16,29 +22,29 @@ public class playerMovement2 : MonoBehaviour {
 
 	void Update (){
 
-		float h = Input.GetAxis("Horizontal");
+		//float h = Input.GetAxis("Horizontal");
 
 
-		if (Input.GetKey ("up")) {//Press up arrow key to move forward on the Y AXIS
+		if (Input.GetKey (up) || Input.GetKey (up2)) {//Press up arrow key to move forward on the Y AXIS
 			transform.Translate (0, playerSpeed * Time.deltaTime, 0);
 			animator.SetBool ("isUp", true);
 		} else {
 			animator.SetBool("isUp", false);
 		}
-		if (Input.GetKey ("down")) {
+		if (Input.GetKey (down) || Input.GetKey(down2)) {
 			transform.Translate (0, -playerSpeed * Time.deltaTime, 0);
 			animator.SetBool ("isDown", true);
 		} else {
 			animator.SetBool ("isDown", false);
 		}
 
-		if (Input.GetKey ("left")) {
+		if (Input.GetKey (left) || Input.GetKey(left2)) {
 			transform.Translate (-playerSpeed * Time.deltaTime, 0, 0);
 			animator.SetBool ("isLeft", true);
 		} else {
 			animator.SetBool ("isLeft", false);
 		}
-		if(Input.GetKey("right"))
+		if(Input.GetKey(right) || Input.GetKey(right2))
 		{
 			transform.Translate(playerSpeed * Time.deltaTime,0 ,0);
 			animator.SetBool("isRight", true);
